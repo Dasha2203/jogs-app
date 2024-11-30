491afb880ee678883e3f753ac804db6f13fa155f
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-11-30 12:28:58 +0300

    feat: add HomePage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,10 +1,42 @@
-import Header from "../../components/layout/Header";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signin } from '../../api';
+import BearIcon from '../../assets/icons/BearIcon';
+import Button from '../../components/Button';
+import Layout from '../../components/layout/Layout';
+import './styles.css';
 
 const HomePage = () => {
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  async function handleClick() {
+    try {
+      const token = await signin();
+      localStorage.setItem('token', token);
+      navigate('/jogs');
+
+    } catch (error) {
+      setError((error as Error).message)
+    }
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      navigate('/jogs');
+    }
+  }, [])
+
   return (
-    <div>
-      <Header />
-    </div>
+    <Layout>
+      <div className="let-me-section">
+        <BearIcon className="let-me-section__img" />
+        <Button as="button" onClick={handleClick}>Let me in</Button>
+        {error && <span className="error">{error}</span>}
+      </div>
+    </Layout>
   )
 };
 
