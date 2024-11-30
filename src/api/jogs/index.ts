0ba4e67ec436079adb3d51e7e6f25d833fe751a9
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-12-01 02:08:59 +0300

    feat: api calls for jog

--- src/api/jogs/index.ts
+++ src/api/jogs/index.ts
@@ -1,11 +1,36 @@
+import { Jog } from '../../models/jogs';
 import { authAxios } from '../../services/axiosInstance';
-import { JogsResponse } from './types';
+import { JogsResponse, AddJogCredential } from './types';
 
 export const getJogs = async () => {
   try {
     const { data } = await authAxios.get<JogsResponse>('/jogs');
 
     return data.jogs;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const createJog = async (value: AddJogCredential) => {
+  try {
+    const { data } = await authAxios.post<JogsResponse>('/jogs', value);
+
+    return data.jogs[0];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const patchJog = async ({ id, distance, date, time }: Jog) => {
+  try {
+    const { data } = await authAxios.patch<JogsResponse>(`/jogs/${id}`, {
+      distance,
+      time,
+      date
+    });
+
+    return data.jogs[0];
   } catch (error) {
     console.log(error);
   }
--- src/api/jogs/types.ts
+++ src/api/jogs/types.ts
@@ -3,3 +3,9 @@ import { Jog } from '../../models/jogs';
 export type JogsResponse = {
   jogs: Jog[];
 };
+
+export type AddJogCredential = {
+  date: string;
+  time: number;
+  distance: number;
+};
